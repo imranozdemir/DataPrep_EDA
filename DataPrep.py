753e7319a229a6f4d73af8324ac3fa9e856b0055
imranozdemir <imranozdemir@hotmail.com.tr>
2022-01-11 20:17:59 +0300

    Define remove outlier function

--- DataPrep.py
+++ DataPrep.py
@@ -8,6 +8,36 @@ from sklearn.preprocessing import LabelEncoder
 
 class MyHelpers:
 
+    def grap_col_names(self, dataframe, cat_th=10, car_th=20):
+       """
+       Grouping numeric columns, categoric columns, categoric but cardinal columns
+
+       :param dataframe: dataframe
+       :param cat_th: int, optinal
+       :param car_th: int, optinal
+       :return: cat_cols, num_cols and cat_but_car as a list
+       """
+       #cat_cols, cat_but_car
+        cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "0"]
+        num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th
+                       and dataframe[col] != "0"]
+        cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "0"]
+        cat_cols= cat_cols + num_but_cat
+        cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+        #num_cols
+        num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "0"]
+        num_cols = [col for col in num_cols if col not in num_but_cat]
+
+        print(f"Observations: {dataframe.shape[0]}")
+        print(f"Variables: {dataframe.shape[1]}")
+        print(f"cat_cols: {len(cat_cols)}")
+        print(f"num_cols: {len(num_cols)}")
+        print(f"cat_but_car: {len(cat_but_car)}")
+        print(f"num_but_cat: {len(num_but_cat)}")
+        return cat_cols, num_cols, cat_but_car
+
+
     def outlier_thresholds(self, dataframe, col_name, q1=0.05, q3=0.95):
         #Computing outliers lower and upper bound using the Inter Quantile Range(IQR)
         #You can change quantiles q1 & q3
@@ -50,4 +80,13 @@ class MyHelpers:
         low_limit, up_limit = outlier_thresholds(dataframe, col_name)
         df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
         return df_without_outliers
+
+    def missing_values_table(self, dataframe, na_name = False):
+        na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+        n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+        ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 1000).sort_values(ascending=False)
+        missing_df = pd.concat([n_miss, np.round(ratio, 1)], axis=1, keys=["n_miss", "ratio"])
+        print(missing_df, end="\n")
+        if na_name:
+           return na_columns
 
